7fb62156c96549d79f96c99882a8ad9f0db9d815
KeithLaiKB <laidelivery@163.com>
2025-03-07 18:29:47 -0500

    changes

--- clion_leetcode_workspace1/mysrc/daimakuangxianglu/binarytree/traversaltree/seq13_t513_FindBottomLeftTreeValue/t513_FindBottomLeftTreeValue.cpp
+++ clion_leetcode_workspace1/mysrc/daimakuangxianglu/binarytree/traversaltree/seq13_t513_FindBottomLeftTreeValue/t513_FindBottomLeftTreeValue.cpp
@@ -10,23 +10,21 @@
 using namespace std;
 
 /**
- * Given the root of a binary tree,
- * return the sum of all left leaves.
+ * Given the root of a binary tree, return all root-to-leaf paths in any order.
  *
  * A leaf is a node with no children.
- * A left leaf is a leaf that is the left child of another node.
  *
+                                    1
+                    |                               |
+                    2                               3
+            |               |               |               |
+            4               null            5               6
+        |       |                         |       |       |       |
+        null   null                       7     null     null    null
+ * Example1:
+ * Input: root = [1,2,3,4,null,5,6,null,null,7]
+ * Output: 7
  *
- *                          1
- *                  |               |
- *                  2               3
- *             |       |       |       |
- *            null    null     6       7
- *
- * 1->2
- * 1->3->6
- * 2+6=8
- * output: 8
  */
 
 class Solution {
@@ -137,63 +135,99 @@ public:
 
     }
 
+    template <typename T>
+    void myOutput_VectorBtB(vector<T>& nums, int st_indx, int ed_indx){
+        for(int i=st_indx;i<=ed_indx;i++){
+            cout<<nums[i]<<"\t";
+            if(i==ed_indx){
+                cout<<endl;
+            }
+        }
+    }
 
 
+    TreeNode* backtracking(TreeNode* root, int &depth, int &maxDepth) {
+        depth ++;
 
+        //limit
+        //为空
+        if(root== nullptr){
+            return nullptr;
+        }
+        //为叶子节点
+        if(root->left == nullptr && root->right == nullptr){
+            // 如果比之前的还大
+            // 则记录当前的depth, 并且 返回当前叶子节点
+            if(depth > maxDepth){
+                maxDepth = depth;
+                cout<<depth<<root->val<<endl;
+                return root;
+            }
+            // 如果不比之前的大
+            // 则不做任何记录
+            else{
+                return nullptr;
+            }
+        }
+
+
+        //不为叶子节点
+        vector<TreeNode*> children1={root->left,root->right};
+
+        TreeNode* rs_left= nullptr;
+        TreeNode* rs_right= nullptr;
+        for(TreeNode *chld : children1){
+
+            //比较左右子树 谁的depth最大
+            if(chld==root->left){
+                rs_left= backtracking(chld,depth,maxDepth);
+            }
+            else if(chld==root->right){
+                rs_right= backtracking(chld,depth,maxDepth);
+            }
+            //
+            //pop
+            depth --;
+        }
+
+        if(rs_left!= nullptr && rs_right== nullptr){
+            return rs_left;
+        }
+        else if(rs_left== nullptr && rs_right!= nullptr){
+            return rs_right;
+        }
+        // 如果左右子树 的返回的ptr都有值
+        // 因为我们便利是从左到右的, 如果右边的pointer是有内容的,
+        // 证明肯定是比左边的深, 右边才有内容的
+        // 所以取的是右边的
+        else if(rs_left!= nullptr && rs_right!= nullptr){
+            return rs_right;
+        }
+        else{
+            return nullptr;
+        }
+
+    }
 
     //时间复杂度: O(n)
     //空间复杂度:
-    //  对于BFS来说  影响 主要是queue的大小
-    //  最低部的叶子节点有可能接近n/2所以
+    //  对于平衡二叉树（例如完全二叉树），递归调用的最大深度是树的高度: O(logn)
+    //      所以是 O(logn)
+    //  对于非平衡的二叉树（例如退化成链表的二叉树），递归调用的最大深度会达到: O(n)
     //      所以是 O(n)
     //
-    // 如果你使用DFS 他每一层不是所有节点都存储的, 所以他就会出现 O(logn)~O(n)的状况
-    // 如果完全平衡二叉树 每一层就存储一个, 总个数 是 2^n=k
-    // 所以 层数n =log(2n)=O(logn)
-    //
-    // 但是这次用的是DFS 变种的先序遍历
-    int backtracking(TreeNode* root) {
-        int rs=0;
-        //limit
-        if(root== nullptr){
-            return rs;
-        }
-/*      //写法1
-        vector<TreeNode*> children1 = {root->left, root->right};
-        for(TreeNode* chld : children1){
-            // 如果   当前节点 不为空 且  lchild 为空
-            if(chld!= nullptr){
-                cout<<root->val<<" "<<chld->val<<endl;
-                //如果当前for的是左叶子, 当前左叶子不为空
-                //  并且 该左叶子的左叶子为空 和  该左叶子的右叶子也为空
-                //      则把 当前左叶子 放进结果 进行累加
-                if(chld == root->left && chld->left== nullptr && chld->right== nullptr){
-                    rs += chld->val;
-                }
-                else{
-                    rs += backtracking(chld);
-                }
-            }
-        }*/
-        //写法2
-        if(root->left!= nullptr && root->left->left== nullptr && root->left->right== nullptr){
-            rs += root->left->val;
+    int findBottomLeftValue_recur(TreeNode* root) {
+        int maxDepth1 = INT_MIN;
+        int depth = 0;
+        TreeNode* rs_node = backtracking(root,depth,maxDepth1);
+        if (rs_node== nullptr){
+            return INT_MIN;
         }
         else{
-            rs += backtracking(root->left);
+            return rs_node->val;
         }
 
-        if(root->right!= nullptr){
-            rs += backtracking(root->right);
-        }
-
-        return rs;
     }
-
-    int sumOfLeftLeaves(TreeNode* root) {
-        return backtracking(root);
-    }
-
 
 
 };
@@ -208,20 +242,19 @@ int main() {
 
     //std::vector<std::optional<int>> intopt_vec1 = {1,2,2,3,4,4,3,5,6,std::nullopt,8,std::nullopt,std::nullopt,6,5};
     //std::vector<std::optional<int>> intopt_vec1 = {3,9,20,std::nullopt,std::nullopt,15,7};
-    std::vector<std::optional<int>> intopt_vec1 = {0,2,4,1,std::nullopt,3,-1,5,1,std::nullopt,6,std::nullopt,8};
-
+    std::vector<std::optional<int>> intopt_vec1 = {1,2,3,4,std::nullopt,5,6,std::nullopt,std::nullopt,7};
     intopt_vec1.reserve(100);
 
 
 
 
     Solution::TreeNode* tree1 = solut1->initLinkedlist_ints(intopt_vec1);
-    //solut1->myOutput_Treenode_int(tree1);
+    solut1->myOutput_Treenode_int(tree1);
 
-    int rs = solut1->sumOfLeftLeaves(tree1);
+    int rs = solut1->findBottomLeftValue_recur(tree1);
     cout<<"result"<<endl;
+    //solut1->myOutput_VectorBtB(rs,0,rs.size()-1);
     cout<<rs<<endl;
-
     return 0;
 }
 
