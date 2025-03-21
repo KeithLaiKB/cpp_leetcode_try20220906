dfe46b95633443d87ac36d18639b4e015072ff1d
KeithLaiKB <laidelivery@163.com>
2025-03-21 14:05:50 -0400

    changes

---
+++ clion_leetcode_workspace1/mysrc/daimakuangxianglu/backtracking/seq1_t77_Combinations/t77_Combinations.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+/**
+ * Given two integers n and k,
+ *
+ *
+ * return all possible combinations of k numbers chosen from the range [1, n].
+ * You may return the answer in any order.
+ *
+ * c(4,2)个组合
+ *
+ * Example 1:
+ * Input: n = 4, k = 2
+ * Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+ * Explanation: There are 4 choose 2 = 6 total combinations.
+ * Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+ *
+ *
+ * Example 2:
+ * Input: n = 1, k = 1
+ * Output: [[1]]
+ * Explanation: There is 1 choose 1 = 1 total combination.
+ *
+ */
+class Solution {
+public:
+    Solution(){
+
+    }
+    ~Solution(){
+
+    }
+    void myOutput_VectorBVectorBintBB(vector<vector<int>>& nums, int st_indx, int ed_indx){
+        for(int i=st_indx;i<=ed_indx;i++){
+            for(int j=0;j<=nums[i].size()-1;j++){
+
+                cout<<nums[i][j]<<"\t";
+                if(j==nums[i].size()-1){
+                    cout<<endl;
+                }
+
+            }
+        }
+    }
+    //
+    vector<int> backtracking(int st_num, int ed_num, int k, vector<int> &vec1,vector<vector<int>> &vec_rs1){
+            //limit
+            if(k==0){
+                vec_rs1.push_back(vec1);
+                return {};
+            }
+            //deal
+
+            //for
+            for(int i=st_num;i<=ed_num;i++){
+                //deal
+                vec1.push_back(i);
+                backtracking(i+1,ed_num,k-1,vec1,vec_rs1);
+                //pop
+                vec1.pop_back();
+            }
+            return {};
+    }
+    //
+    //
+    // 时间复杂度： O(C(n,k) × k)
+    //   关于push_back和pop_back操作：
+    //      虽然单次操作可以看作O(1)
+    //      但是对于每个组合，我们仍需要进行k次这样的操作
+    //
+    //      因此总时间复杂度应为：O(C(n,k) × k)，即O(n!/(k!(n-k)!) × k)
+    //      这可能会简化为O(C(n,k))，如果我们认为k相对于n和组合数量来说足够小，可以被视为常数。但严格来说，应该是O(C(n,k) × k)。
+    //
+    //
+    // 空间复杂度： O(C(n,k) × k)
+    //
+    //    空间复杂度需要考虑：
+    //    1.递归栈深度：最深为k层，因此为O(k)
+    //    2.存储结果的空间：有C(n,k)个结果，每个结果占用O(k)空间，因此为O(k × C(n,k))
+    //
+    //    总空间复杂度为 O(k) + O(k × C(n,k)) = O(k × C(n,k))，因为结果存储空间远大于递归栈空间。
+    //
+    //
+    //                                  [1,2,3,4]
+    //            |                       |                 |           |
+    //           [1]                     [2]                [3]         [4]
+    //       |    |    |               |    |                |
+    //      [2]  [3]  [4]             [3]  [4]              [4]
+    //
+    //
+    // 这就是回溯 把所有可能性都回溯一遍
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> vec_vecBintB1 = {};
+
+        for(int i=1;i<=n;i++){
+            vector<int> vec_tmp={i};
+            backtracking(i+1, n,  k-1, vec_tmp,vec_vecBintB1);
+        }
+
+        return vec_vecBintB1;
+
+    }
+};
+
+
+int main() {
+    cout << "result" << endl;
+    Solution* solut1 = new Solution();
+
+    //vector<int>* p_intvec = new vector<int>({-1,0,3,5,9,12});
+   // vector<int> intvec1 ={-1,0,3,5,9,12};
+
+
+    //int mytarget = 5;
+
+    int indx_rs1 = -1;
+
+    int n =4;
+    int k = 2;
+    vector<vector<int>> rs1 = solut1->combine(n,k);
+    cout<<"position index:"<<indx_rs1<<endl;
+
+    solut1->myOutput_VectorBVectorBintBB(rs1,0,rs1.size());
+
+    return 0;
+}
+
+
---
+++ clion_leetcode_workspace1/mysrc/daimakuangxianglu/greedy/seq1_t455_AssignCookies/t455_AssignCookies.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+/**
+ * Assume you are an awesome parent and want to give your children some cookies.
+ * But, you should give each child at most one cookie.
+ * Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with;
+ * and each cookie j has a size s[j]. If s[j] >= g[i],
+ * we can assign the cookie j to the child i, and the child i will be content.
+ * Your goal is to maximize the number of your content children and output the maximum number.
+ *
+ *
+ * Example1:
+ * Input: g = [1,2,3], s = [1,1]
+ * Output: 1
+ * Explanation:
+ *      You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3.
+ *      And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
+ *      You need to output 1.
+ *
+ * Example2:
+ * Input: g = [1,2], s = [1,2,3]
+ * Output: 2
+ * Explanation:
+ *      You have 2 children and 3 cookies. The greed factors of 2 children are 1, 2.
+ *      You have 3 cookies and their sizes are big enough to gratify all of the children,
+ *      You need to output 2.
+ *
+ */
+class Solution {
+public:
+    Solution(){
+
+    }
+    ~Solution(){
+
+    }
+    // 时间复杂度：O(log n)
+    // 空间复杂度：O(1)
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+
+    }
+};
+
+
+int main() {
+    cout << "result" << endl;
+    Solution* solut1 = new Solution();
+
+    vector<int>* p_intvec = new vector<int>({-1,0,3,5,9,12});
+    int mytarget = 5;
+
+    int indx_rs1 = -1;
+    indx_rs1 = solut1->search(*p_intvec,mytarget);
+    cout<<"position index:"<<indx_rs1<<endl;
+
+
+    delete p_intvec;
+    return 0;
+}
+
+
