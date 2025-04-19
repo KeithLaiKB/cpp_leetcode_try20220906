eb537da9c196009625b06db0602745548331ac59
KeithLaiKB <laidelivery@163.com>
2025-04-18 21:51:24 -0400

    changes

--- clion_leetcode_workspace1/mysrc/daimakuangxianglu/greedy/seq1_t455_AssignCookies/t455_AssignCookies.cpp
+++ clion_leetcode_workspace1/mysrc/daimakuangxianglu/greedy/seq1_t455_AssignCookies/t455_AssignCookies.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <unordered_map>
 #include <string>
+#include <algorithm>
+
 using namespace std;
 
 /**
@@ -10,7 +12,9 @@ using namespace std;
  * Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with;
  * and each cookie j has a size s[j]. If s[j] >= g[i],
  * we can assign the cookie j to the child i, and the child i will be content.
- * Your goal is to maximize the number of your content children and output the maximum number.
+ *
+ * Your goal is to maximize the number of your content children
+ * and output the maximum number.
  *
  *
  * Example1:
@@ -38,10 +42,34 @@ public:
     ~Solution(){
 
     }
+
+    bool static mycomp(const int &a, const int &b){
+        return a<b;
+    }
+
     // 时间复杂度：O(log n)
     // 空间复杂度：O(1)
-    int findContentChildren(vector<int>& g, vector<int>& s) {
+    int findContentChildren(vector<int>& child, vector<int>& cookie) {
+        if(child.size()==0 || cookie.size()==0){
+            return 0;
+        }
 
+        sort(child.begin(),child.end(),mycomp);
+        sort(cookie.begin(),cookie.end(),mycomp);
+
+        int child_idx=0;
+        int rs = 0;
+
+        //先把人喂抱, 当前人喂饱了才能看下一个人
+        // 所以 人的遍历 是不能在外面这个for循环的
+        // cookie 会用 外面这个循环
+        for(int cookie_idx=0;cookie_idx<=cookie.size()-1;cookie_idx++){
+            if(child_idx<=child.size()-1 && child[child_idx]<=cookie[cookie_idx]){
+                ++rs;
+                ++child_idx;
+            }
+        }
+        return rs;
     }
 };
 
@@ -50,15 +78,14 @@ int main() {
     cout << "result" << endl;
     Solution* solut1 = new Solution();
 
-    vector<int>* p_intvec = new vector<int>({-1,0,3,5,9,12});
-    int mytarget = 5;
+    vector<int> vec_int1={1,2,3};
+    vector<int> vec_int2={1,1};
+    //int mytarget = 5;
 
-    int indx_rs1 = -1;
-    indx_rs1 = solut1->search(*p_intvec,mytarget);
-    cout<<"position index:"<<indx_rs1<<endl;
-
-
-    delete p_intvec;
+    int rs1 = -1;
+    rs1 = solut1->findContentChildren(vec_int1,vec_int2);
+    cout<<"result:"<<endl;
+    cout<<rs1<<endl;
     return 0;
 }
 
